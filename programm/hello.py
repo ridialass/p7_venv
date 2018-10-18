e92c8047f46539fe8d5fc79cc712e82d5de394b4
ridialass <ridialass@gmail.com>
2018-10-18 15:53:51 +0200

    +flask

---
+++ programm/hello.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+from flask import Flask
+#déclare le serveur flask
+app = Flask(__name__)
+#crée la route web de la racine du site
+#et la lie à la fonction hello
+@app.route("/")
+def hello():
+   return "Hello World!"
+if __name__ == "__main__":
+#lance le serveur Flask
+   app.run(host="0.0.0.0",debug=True)
